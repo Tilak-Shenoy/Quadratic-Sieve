ccf47fb6009c83141684771417a11ae9c2a44224
Tilak N Shenoy <tilaknshenoy@gmail.com>
2018-06-13 22:11:59 +0530

    Initial Commit

---
+++ ofd.c
@@ -0,0 +1,19 @@
+#include <linux/module.h>
+#include <linux/version.h>
+#include <linux/kernel.h>
+
+static int __init ofd_init(void){
+	printk(KERN_INFO "Hello: ofd registered");
+	return 0;
+}
+
+static void __exit ofd_exit(void){
+	printk(KERN_INFO "Bye: ofd unregistered");
+}
+
+module_init(ofd_init);
+module_exit(ofd_exit);
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Aayush Jain");
+MODULE_DESCRIPTION("Our First Driver");
